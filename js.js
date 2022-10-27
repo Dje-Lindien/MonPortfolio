fff944972ed4da489785e44ad7c482086fefcc93
jeremy-breuillard <72251531+Dje-Lindien@users.noreply.github.com>
2022-10-27 18:47:33 +0200

    Delete js.js

--- js.js
+++
@@ -1,6 +0,0 @@
-const link = document.querySelector('imgBx');
-const detail = document.querySelector('.detail');
-
-link.addEventListener('click', () => {
-    detail.classList.toggle('is-visible');
-})
